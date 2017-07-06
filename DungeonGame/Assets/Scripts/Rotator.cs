48e51ac236c21d5c1caafb11d4aa35f695f8fbe4
Akhil Batra <akbatra@akhilbatra.com>
2017-07-05 17:59:15 -0700

    basic game!

---
+++ DungeonGame/Assets/Scripts/Rotator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(10 + Random.Range(5, 20), 20 + Random.Range(5, 20), 30 + Random.Range(5, 20)) * Time.deltaTime);
+    }
+}
